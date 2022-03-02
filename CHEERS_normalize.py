67cc4e447629552efff302577f1773df82966cec
Nikhil Milind <nikhilmilind@gmail.com>
2022-03-02 12:26:51 +0000

    Rewrote normalization step in Python 3

--- CHEERS_normalize.py
+++ CHEERS_normalize.py
@@ -1,194 +1,195 @@
-'''
-:File: CHEERS_normalize.py
-:Author: Blagoje Soskic, Wellcome Sanger Institute, <bs11@sanger.ac.uk>
-:Last updated: 20/1/2019
+#!/usr/bin/env python
 
-This script is used to normalize counts within peaks in the dataset:
-1) load output of featureCounts *_ReadsInPeaks.txt (each txt file is an analyzed sample that contains 4 columns without header: chr   start   end count )
-2) correct counts per peak for library size by scaling it to the sample with the largest sum of counts
-3) remove the bottom 10th percentile of peaks with the lowest read counts
-4) quantile normalize the library size-corrected peak counts
-5) Euclidean normalization to obtain a cell type specificity score
-
-outputs :
-outputPrefix_normToMax.txt
-outputPrefix_normToMax_quantileNorm.txt
-outputPrefix_normToMax_quantileNorm_euclideanNorm.txt
-
-Usage:
-    python CHEERS_normalize.py --input ~/peak/counts/per/sample/ --prefix prefix --outdir ~/output/directory/
-
-
-Copyright (C) 2019  Blagoje Soskic
-
-This file is part of CHEERS code.
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in
-    all copies or substantial portions of the Software.
-
-BY USING THE SOFTWARE YOU ACKNOWLEDGE THAT YOU HAVE READ AND UNDERSTAND THE
-TERMS OF USE BELOW. 
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-
-THIS SOFTWARE IS TO BE USED AS A RESEARCH TOOL ONLY. THE SOFTWARE TOOL SHALL
-NOT BE USED AS A DIAGNOSTIC DECISION MAKING SYSTEM AND MUST NOT BE USED TO
-MAKE A CLINICAL DIAGNOSIS OR REPLACE OR OVERRULE A LICENSED HEALTH CARE
-PROFESSIONAL'S JUDGMENT OR CLINICAL DIAGNOSIS. ANY ACTION THE RESEARCHER TAKES
-IN RESPONSE TO THE INFORMATION CONTAINED WITHIN IS AT THE RESEARCHER'S
-DISCRETION ONLY.
-
-'''
-
-#import libraries
-import os
+import argparse
+import functools
 import glob
-import re
+import os
+import sys
+
 import numpy as np
 import pandas as pd
-import argparse
 
 
-#parse arguments
-parser = argparse.ArgumentParser(description = "data processing for CHEERS disease enrichment", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("--input", help = "path to files with read counts per peak")
-parser.add_argument("--prefix", help = "file prefix")
-parser.add_argument("--outdir", help = "directory where to output results")
-args = parser.parse_args()
+def parse_arguments():
 
-#define functions
-def quantile_norm(normData):
-    regions = normData.loc[:, :'end']
-    normDataClean = normData.drop(['chr', 'start', 'end'], axis=1)
-    rank_mean = normDataClean.stack().groupby(normDataClean.rank(method='first').stack().astype(int)).mean()
-    normDataCleanQuantile = normDataClean.rank(method='min').stack().astype(int).map(rank_mean).unstack()
-    normDataCleanQuantileFinal = pd.concat([regions, normDataCleanQuantile], axis=1)
-    return normDataCleanQuantileFinal
+    '''
+    Parse and validate command line arguments.
 
-def euclidean_norm(normDataCleanQuantileFinal):
-    normDataQuantile = normDataCleanQuantileFinal.drop(['chr', 'start', 'end'], axis=1)
-    normDataSquare = np.square(normDataQuantile)
-    normDataQuantile['eucl_norm'] = np.sqrt(normDataSquare.sum(axis=1))
-    normDataQuantileEucl = normDataQuantile.iloc[:, 0:].div(normDataQuantile.eucl_norm, axis=0)
-    dim = normDataQuantileEucl.shape[1] - 1
-    final = normDataQuantileEucl.iloc[:, :dim]
-    return final
+    :return: A namespace containing the command line argument values.
+    '''
 
-#set variables
-filesNames = []
-filesSums = []
-filesData = {}
-peakSums = []
+    parser = argparse.ArgumentParser(description='Data processing for CHEERS disease enrichment')
+    parser.add_argument('prefix', help='File prefix')
+    parser.add_argument('outdir', help='Directory for result output')
+    parser.add_argument('input', nargs='*', help='Pattern of files with read counts per peak')
+    args = parser.parse_args()
 
-#chrs to keep for the analysis
-chrList = ['chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9', 'chr10', 'chr11', 'chr12', 'chr13', 'chr14', 'chr15', 'chr16', 'chr17', 'chr18', 'chr19', 'chr20', 'chr21', 'chr22', 'chrX']
+    if not args.input:
+        print(f'Input did not match any valid paths to files', file=sys.stderr)
+        exit(2)
+    
+    if not os.path.isdir(args.outdir):
+        print(f'{args.outdir} is not a valid path to a directory', file=sys.stderr)
+        exit(2)
 
-# open and load all counts per files
-for filename in glob.glob(os.path.join(args.input, '*_ReadsInPeaks.txt')):
-    file = open(filename, "r")
-    fileText = file.read()
-    file.close()
-    fileTextLines = fileText.split("\n") #split lines in file
-    countSum = 0 # define count sum
-    for line in fileTextLines:
-        if (line): #check if line exists
-            lineParts = line.split("\t")
-            if (lineParts[0] in chrList):
-                key = lineParts[0] + '-' + lineParts[1] + '-' + lineParts[2] #create key based on chr, start and end
-                count = float(lineParts[3])
-                if not(key in filesData):
-                    filesData[key] = [] #if key doesn't exist create it
-                countSum += count
-                filesData[key].append(count) #create dictionary with key being location and value being read counts
-    filesNames.append(os.path.basename(filename)) #create list of sums
-    filesSums.append(countSum) #create list of names
+    return args
 
-'''
-Normalize counts to account for sequencing depth.
-'''
-#create norm factor
-maxSum = max(filesSums) #find max sum
-filesSums[:] = [maxSum / x for x in filesSums] #divide every max sum with sum of reads per condition
 
-#normalize each count
-for key in filesData:
-    counts = filesData[key]
-    normalizedCounts = []
-    for i in range(0, len(counts)):
-        normalizedCounts.append(counts[i] * filesSums[i])
-    filesData[key] = normalizedCounts
+def read_files(files):
 
-'''
-remove bottom 10th percentile of the peaks
-'''
-for key in filesData:
-    peakCounts = filesData[key]
-    sumPeakCounts = sum(filesData[key])
-    peakSums.append(sumPeakCounts)
-peakSumsArray = np.array(peakSums)
-bottom = np.percentile(peakSumsArray, 10)
+    '''
+    Read input count files and generate a data frame.
 
-for (key, value) in filesData.items():
-    if sum(value) < bottom:
-        del filesData[key]
+    :param files: A list of file paths.
+    :return: A count matrix, where each column is for one condition and each row represents a peak.
+    '''
 
-#create headers for the outputs
-normalizedCountsOutput = 'chr' + '\t' + 'start' + '\t' + 'end' + '\t'
-for name in filesNames:
-    name = re.sub('_ReadsInPeaks\.txt$', '', name)
-    normalizedCountsOutput += str(name) + '\t'
-normalizedCountsOutput += '\n'
+    data_in_dfs = list()
 
-#fill table with the normalized counts
-for key in filesData:
-    keyParts = key.split('-')
-    normalizedCountsOutput += keyParts[0] + '\t' + keyParts[1] + '\t' + keyParts[2] + '\t'
-    normalizedCounts = filesData[key]
-    for count in normalizedCounts:
-        if count == normalizedCounts[:-1]:
-            normalizedCountsOutput += str(count)
-        else:
-            normalizedCountsOutput += str(count) + '\t'
-    normalizedCountsOutput += '\n'
+    for file_name in files:
 
-#create output for counts normalised for library size and bottom 10th percentile peaks removed
-fileName1 = str(args.outdir) + str(args.prefix) + '_counts_normToMax.txt'
+        if not os.path.isfile(file_name):
+            print(f'Could not open file {file_name}', file=sys.stderr)
+            exit(1)
+        
+        condition_name = os.path.basename(file_name).split('.')[0]
 
-text_file = open(fileName1, "w")
-text_file.write(normalizedCountsOutput)
-text_file.close()
+        data_in = pd.read_csv(file_name, sep='\t', header=None)
+        data_in.columns = ['chr', 'start', 'end', condition_name]
 
-'''
-quantile normalize the library size-corrected peak counts to make comparisons between different peaks possible
-'''
-#quantile normalisation and output
-normData = pd.read_csv(fileName1, sep='\t')
-normDataQuantile = quantile_norm(normData)
-fileName2 = str(args.outdir) + str(args.prefix) + '_counts_normToMax_quantileNorm.txt'
-normDataQuantile.to_csv(fileName2, header=1, index=None, sep='\t')
+        data_in_dfs.append(data_in)
 
-'''
-To assess specificity score perform euclidean normalization
-'''
-#euclidean normalization and output
-euclideanNorm = euclidean_norm(normDataQuantile)
-regions = normDataQuantile.loc[:, :'end']
-euclideanNorm = euclideanNorm.round(4)
-euclideanNormFinal = pd.concat([regions, euclideanNorm], axis=1)
+    count_data = functools.reduce(
+        lambda left, right: pd.merge(left, right, on=['chr', 'start', 'end'], how='inner'),
+        data_in_dfs
+    )
 
-fileName3 = str(args.outdir) + str(args.prefix) + '_counts_normToMax_quantileNorm_euclideanNorm.txt'
+    count_data.set_index(count_data.chr + '-' + count_data.start.map(str) + '-' + count_data.end.map(str), inplace=True)
+   
+    count_data.drop(['chr', 'start', 'end'], axis=1, inplace=True)
 
-euclideanNormFinal.to_csv(fileName3, header=1, index=None, sep='\t')
+    return count_data
+
+
+def normalize_counts(count_data):
+
+    '''
+    Normalize counts to account for sequencing depth.
+
+    :param count_data: A data frame from Peak ID to list of counts across conditions.
+    :return: Normalized count data.
+    '''
+
+    count_sums = count_data.sum(axis=0)
+
+    norm_factors = count_sums.max() / count_sums
+
+    return count_data * norm_factors
+
+
+def drop_peaks(norm_data):
+
+    '''
+    Drops the bottom 10th percentile of peaks by total count.
+
+    :param norm_data: A normalized peak count matrix.
+    :return: A normalized peak count matrix with the top 90% of peaks based on total count.
+    '''
+
+    peak_sums = norm_data.sum(axis=1)
+
+    tenth_percentile = np.percentile(peak_sums, 10)
+
+    return norm_data[peak_sums >= tenth_percentile]
+
+
+def quantile_norm(norm_data):
+
+    '''
+    Quantile normalize each condition's peak count distributions.
+
+    :param norm_data: A normalized peak count matrix.
+    :return: A quantile-normalized peak count matrix.
+    '''
+
+    rank_mean = norm_data.stack().groupby(norm_data.rank(method='first').stack().astype(int)).mean()
+
+    return norm_data.rank(method='min').stack().astype(int).map(rank_mean).unstack()
+
+
+def euclidean_norm(norm_data):
+
+    '''
+    Normalize each peak's count by the euclidean norm across all conditions.
+
+    :param norm_data: A normalized peak count matrix.
+    :return: A Euclidean-normalized peak count matrix.
+    '''
+
+    norm_data_squared = np.square(norm_data)
+
+    euclidean_norm = np.sqrt(norm_data_squared.sum(axis=1))
+
+    return norm_data.div(euclidean_norm, axis=0)
+
+
+def write_matrix(matrix, file_path):
+
+    '''
+    Writes any matrix generated in this file. The matrix should have an index of peak IDs.
+
+    :param matrix: A matrix to write out.
+    :param file_path: The file path for the matrix output.
+    '''
+
+    out_mtx = matrix.copy()
+
+    peak_info = pd.Series(out_mtx.index).str.split('-', expand=True)
+    peak_info.columns = ['chr', 'start', 'end']
+    peak_info.start = peak_info.start.astype(int)
+    peak_info.end = peak_info.end.astype(int)
+    peak_info.index = out_mtx.index
+
+    out_mtx = pd.merge(peak_info, out_mtx, left_index=True, right_index=True)
+
+    out_mtx.sort_values(['chr', 'start', 'end'], inplace=True)
+
+    out_mtx.to_csv(file_path, index=None, sep='\t')
+
+
+def main():
+
+    # Read command line arguments
+    args = parse_arguments()
+
+    # Generate one unified count matrix from input files
+    count_data = read_files(args.input)
+
+    # Normalize data based on library size
+    norm_data = normalize_counts(count_data)
+
+    # Drop bottom 10th percentile of peaks
+    norm_data = drop_peaks(norm_data)
+
+    # Write library-normalized data to disk
+    norm_data_path = os.path.join(args.outdir, f'{args.prefix}_counts_normToMax.txt')
+    write_matrix(norm_data, norm_data_path)
+
+    # Perform quantile normalization
+    quantile_norm_data = quantile_norm(norm_data)
+
+    # Write quantile-normalized data to disk
+    quantile_norm_data_path = os.path.join(args.outdir, f'{args.prefix}_counts_normToMax_quantileNorm.txt')
+    write_matrix(quantile_norm_data, quantile_norm_data_path)
+
+    # Perform Euclidean normalization to generate specificity scores for peaks
+    euclidean_norm_data = euclidean_norm(quantile_norm_data)
+
+    # Write Euclidean-normalized data to disk
+    euclidean_norm_data_path = os.path.join(args.outdir, f'{args.prefix}_counts_normToMax_quantileNorm_euclideanNorm.txt')
+    write_matrix(euclidean_norm_data.round(4), euclidean_norm_data_path)
+
+
+if __name__ == '__main__':
+
+    main()
